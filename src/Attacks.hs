96f22f7b8af529b2709524695d5fd486eafe0c39
Matt Hunzinger <matthunz2@gmail.com>
2023-07-15 22:39:59 -0400

    Fix pawn attacks and add checkers function

--- src/Attacks.hs
+++ src/Attacks.hs
@@ -12,8 +12,8 @@ import Types (Color (..))
 
 pawnAttacks :: Color -> Square -> BitBoard
 pawnAttacks color square = BitBoard $ case color of
-  Black -> $(lift (initSteppingAttacks [7, 9])) !! fromEnum square
-  White -> $(lift (initSteppingAttacks [-7, -9])) !! fromEnum square
+  Black -> $(lift (initSteppingAttacks [-7, -9])) !! fromEnum square
+  White -> $(lift (initSteppingAttacks [7, 9])) !! fromEnum square
 
 knightAttacks :: Square -> BitBoard
 knightAttacks square = BitBoard $ $(lift (initSteppingAttacks [17, 15, 10, 6, -17, -15, -10, -6])) !! fromEnum square
--- src/Board.hs
+++ src/Board.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE NumericUnderscores #-}
 
-module Board (attackersTo, standard, Board (..), legalMoves, Move (..), move, fen) where
+module Board (attackersTo, standard, Board (..), legalMoves, Move (..), move, fen, checkers) where
 
 import Attacks
 import BitBoard
@@ -10,8 +10,10 @@ import Data.Word (Word64)
 import Square
 import Types
 
+--- Role of a piece
 data Role = Pawn | Knight | Bishop | Rook | Queen | King deriving (Show, Eq)
 
+--- Move to perform on a chess board
 data Move
   = NormalMove
       { normalRole :: Role,
@@ -22,6 +24,7 @@ data Move
   | EnPassantMove Square Square
   deriving (Show)
 
+--- Chess board
 data Board = Board
   { getTurn :: Color,
     getPawns :: BitBoard,
@@ -36,6 +39,7 @@ data Board = Board
   }
   deriving (Show)
 
+--- Output the FEN text of a board
 fen :: Board -> String
 fen board = concatMap withRank (reverse $ enumFromTo Rank1 Rank8)
   where
@@ -50,6 +54,7 @@ fen board = concatMap withRank (reverse $ enumFromTo Rank1 Rank8)
              in (0, acc ++ s ++ [pieceChar piece])
           Nothing -> (count + 1, acc)
 
+--- Create a board with the standard starting position
 standard :: Board
 standard =
   Board
@@ -64,6 +69,7 @@ standard =
     (BitBoard 0xffff)
     (BitBoard 0xffff_0000_0000_ffff)
 
+--- Perform a move on the board
 move :: Move -> Board -> Board
 move m board = case m of
   NormalMove role from to capture ->
@@ -82,13 +88,15 @@ move m board = case m of
       mv bb = moveSquare bb from to
   EnPassantMove from to -> error "TODO"
 
+--- Returns the attackers of a color to the given square
 attackersTo :: Square -> Color -> Board -> BitBoard
 attackersTo square attacker board =
-  let BitBoard pawnAttackers = pawnAttacks attacker square
-      BitBoard pawns = getPawns board
-      BitBoard knightAttackers = knightAttacks square
-      BitBoard knights = getKnights board
-   in BitBoard $ (pawnAttackers .&. pawns) .|. (knightAttackers .&. knights)
+  (pawnAttacks attacker square .&. getPawns board) <> (knightAttacks square .&. getKnights board)
+
+checkers :: Board -> BitBoard
+checkers board = case squares $ our King board of
+  [] -> BitBoard 0
+  (sq : _) -> attackersTo sq (toggle $ getTurn board) board
 
 roleAt :: Square -> Board -> Maybe Role
 roleAt square board
@@ -134,6 +142,15 @@ byColor :: Color -> Board -> BitBoard
 byColor Black = getBlack
 byColor White = getWhite
 
+byRole :: Role -> Board -> BitBoard
+byRole role = case role of
+  Pawn -> getPawns
+  Knight -> getKnights
+  Bishop -> getBishops
+  Rook -> getRooks
+  Queen -> getQueens
+  King -> getKings
+
 -- Returns the pieces of the current player
 us :: Board -> BitBoard
 us board = byColor (getTurn board) board
@@ -141,6 +158,9 @@ us board = byColor (getTurn board) board
 -- Returns the pieces of the next player
 them :: Board -> BitBoard
 them board = byColor (toggle $ getTurn board) board
+
+our :: Role -> Board -> BitBoard
+our role board = us board .&. byRole role board
 
 pawnMoves :: BitBoard -> Board -> [Move]
 pawnMoves target board =
@@ -152,14 +172,14 @@ pawnMoves target board =
       pawnCaptures dir =
         let backRanks = BitBoard 0xff00_0000_0000_00ff
             captures = translate dir (us board .&. getPawns board) .&. them board .&. target
-            f to =
+            moveTo to =
               NormalMove
                 { normalRole = Pawn,
                   normalTo = to,
                   normalFrom = toEnum (fromEnum to - offset dir),
                   normalCapture = roleAt to board
                 }
-         in map f (squares $ captures .&. backRanks)
+         in map moveTo (squares $ captures .&. backRanks)
    in pawnCaptures east ++ pawnCaptures west
 
 knightMoves :: BitBoard -> Board -> [Move]
